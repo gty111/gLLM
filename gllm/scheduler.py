4446e875f505f1b20438d8b440ffc88008563536
guoty <guoty9@mail2.sysu.edu.cn>
2024-08-05 04:31:47 +0800

    Remove scheduled seqs from decode lists

--- gllm/scheduler.py
+++ gllm/scheduler.py
@@ -7,9 +7,9 @@ from gllm.model_runner import ModelRunner
 class Scheduler:
     def __init__(self, model_runner: ModelRunner, max_decode_seqs: int, max_batch_tokens: int, ratio_threshold_free_pages: float) -> None:
         self.model_runner = model_runner
-        self.prompt_lists: List[Sequence] = []
-        self.decode_lists: List[Sequence] = []
-        self.finish_lists: List[Sequence] = []
+        self.prompt_lists: List[Sequence] = [] # seqs to prefill
+        self.decode_lists: List[Sequence] = [] # seqs to decode
+        self.finish_lists: List[Sequence] = [] # seqs finished
 
         self.max_decode_seqs = max_decode_seqs
         self.max_batch_tokens = max_batch_tokens
@@ -25,7 +25,7 @@ class Scheduler:
         # prompt
         num_free_pages = self.model_runner.memory_manager.get_num_free_pages()
         if len(self.prompt_lists) != 0 and (
-                num_free_pages > self.num_threshold_free_pages and len(self.decode_lists) < self.max_decode_seqs):
+                num_free_pages > self.num_threshold_free_pages and len(schedule_lists) < self.max_decode_seqs):
             cu_seqs_len = 0
             for seq in self.prompt_lists:
                 if cu_seqs_len + len(
@@ -33,7 +33,6 @@ class Scheduler:
                         cu_seqs_len + len(seq.token_ids)) < (
                             num_free_pages - self.num_threshold_free_pages) * self.model_runner.memory_manager.page_size:
                     cu_seqs_len += len(seq.token_ids)
-                    self.decode_lists.append(seq)
                     schedule_lists.append(seq)
             for seq in schedule_lists:
                 self.prompt_lists.remove(seq)
@@ -42,7 +41,8 @@ class Scheduler:
         if len(schedule_lists) == 0:
             decode_batch_size = min(
                 self.max_decode_seqs, num_free_pages)
-            schedule_lists.extend(self.decode_lists[:decode_batch_size])
+            schedule_lists = self.decode_lists[:decode_batch_size]
+            self.decode_lists = self.decode_lists[decode_batch_size:]
 
         assert len(schedule_lists) != 0 and "Try to increase ratio_threshold_free_pages"
 
@@ -61,14 +61,12 @@ class Scheduler:
             if not seqs[i].computed_prompt:
                 seqs[i].computed_prompt = True
         # check finished seqs
-        finish_lists_each = []
-        for seq in self.decode_lists:
+        for seq in seqs:
             if seq.token_ids[-1] in self.model_runner.model.finish_tokens or len(seq.token_ids) - seq.prompt_len >= seq.output_len:
-                finish_lists_each.append(seq)
                 self.finish_lists.append(seq)
                 self.model_runner.free_kv_cache(seq)
-        for seq in finish_lists_each:
-            self.decode_lists.remove(seq)
+            else:
+                self.decode_lists.append(seq)
 
     def has_seqs(self):
         return len(self.prompt_lists) + len(self.decode_lists) != 0
