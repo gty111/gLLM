220408ee162bb207a1adc1fafc481ac84779caef
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-02-26 12:39:31 +0800

    Make GPU process can run without schedule (#14)

--- gllm/scheduler.py
+++ gllm/scheduler.py
@@ -15,9 +15,8 @@ class SchedulerOutput:
 
 # Only used for decode
 class DeltaSchedulerOutput:
-    def __init__(self, free_indices: List[int], keep_indices: List[int], delta_schedule_list: List[int]):
+    def __init__(self, free_indices: List[int], delta_schedule_list: List[int]):
         self.free_indices = free_indices # gpu process => schedule process
-        self.keep_indices = keep_indices # gpu process => schedule process
         self.delta_schedule_list = delta_schedule_list # schedule process => gpu process
 
 
@@ -29,16 +28,17 @@ class Scheduler:
         self.finish_lists: List[Sequence] = []  # seqs finished
 
         self.decode_batch = SchedulerOutput([])
-        self.delta_scheduler_output = DeltaSchedulerOutput([], [], [])
+        self.delta_scheduler_output = DeltaSchedulerOutput([], [])
 
         self.max_decode_seqs = max_decode_seqs
         self.max_batch_tokens = max_batch_tokens
         self.num_threshold_free_pages = int(
             total_num_free_pages * ratio_threshold_free_pages)
 
+        # only used for delta
         self.num_schedule_prefill = 0
-        self.num_schedule_decode = 0
-
+        self.schedule_decode = True
+        
         self.total_num_free_pages = total_num_free_pages
         self.num_free_pages = total_num_free_pages
         self.page_size = page_size
@@ -65,7 +65,7 @@ class Scheduler:
 
         # prompt
         if len(self.prompt_lists) != 0 and (
-                self.num_free_pages > self.num_threshold_free_pages):
+                self.num_free_pages > self.num_threshold_free_pages) and (not delta or delta and self.num_schedule_prefill <= 1):
             prefill_schedule_lists: List[Sequence] = []
             cu_seqs_len = 0
             for seq in self.prompt_lists:
@@ -77,11 +77,9 @@ class Scheduler:
             for seq in prefill_schedule_lists:
                 self.prompt_lists.remove(seq)
             self.num_schedule_prefill += 1
-
             return SchedulerOutput(prefill_schedule_lists)
 
         # decode
-        self.num_schedule_decode += 1
         if not delta:
             decode_batch_size = min(
                 self.max_decode_seqs, self.num_free_pages*self.page_size, len(self.decode_lists))
@@ -89,6 +87,8 @@ class Scheduler:
             self.decode_lists = self.decode_lists[decode_batch_size:]
             return SchedulerOutput(schedule_lists)
         else:
+            if not self.schedule_decode:
+                return DeltaSchedulerOutput([],[])
             assert self.num_free_pages*self.page_size > len(self.decode_batch.schedule_lists)
 
             delta_batch_size = min(self.num_free_pages*self.page_size, self.max_decode_seqs -
@@ -99,7 +99,10 @@ class Scheduler:
             self.decode_lists = self.decode_lists[delta_batch_size:]
 
             assert (len(self.decode_batch.schedule_lists) +
-                    delta_batch_size) != 0 and "Try to increase ratio_threshold_free_pages"
+                    delta_batch_size != 0 or self.num_schedule_prefill) and "Try to increase ratio_threshold_free_pages"
+            # we only schedule decode when GPU has stepped once on last decode batch
+            if not len(self.delta_scheduler_output.delta_schedule_list) == 0:
+                self.schedule_decode = False 
             return self.delta_scheduler_output
 
 
@@ -110,24 +113,24 @@ class Scheduler:
                     self.finish_lists.append(seq)
                 else:
                     self.decode_lists.append(seq)
-            if schedulerOutput.computed_prompt:
-                self.num_schedule_decode -= 1
-            else:
-                self.num_schedule_prefill -= 1
         else:
+            
             if isinstance(schedulerOutput, SchedulerOutput):  # prefill
-                self.num_schedule_prefill -= 1
                 for idx in schedulerOutput.free_indices:
                     self.finish_lists.append(schedulerOutput.schedule_lists[idx])
-                for idx in schedulerOutput.keep_indices:
+                keep_indices = list(set(range(len(schedulerOutput.schedule_lists)))-set(schedulerOutput.free_indices))
+                for idx in keep_indices:
                     self.decode_lists.append(schedulerOutput.schedule_lists[idx])
+                self.num_schedule_prefill -= 1
             elif isinstance(schedulerOutput, DeltaSchedulerOutput):  # decode
-                self.num_schedule_decode -= 1
+                self.schedule_decode = True
                 for i in schedulerOutput.free_indices:
                     self.finish_lists.append(self.decode_batch.schedule_lists[i])
+                keep_indices = list(set(range(len(self.decode_batch.schedule_lists)))-set(schedulerOutput.free_indices))
                 self.decode_batch.schedule_lists = [
-                    self.decode_batch.schedule_lists[i] for i in schedulerOutput.keep_indices]
-                for i,idx in enumerate(schedulerOutput.keep_indices):
+                    self.decode_batch.schedule_lists[i] for i in keep_indices]
+                keep_indices_token = list(set(range(len(next_tokens)))-set(schedulerOutput.free_indices))
+                for i,idx in enumerate(keep_indices_token):
                     self.decode_batch.schedule_lists[i].token_ids.append(next_tokens[idx])
 
     def can_schedule_prefill(self):
@@ -137,8 +140,7 @@ class Scheduler:
         return self.can_schedule_prefill() or len(self.decode_lists) + len(self.decode_batch.schedule_lists) != 0
 
     def has_seqs(self):
-        return len(self.prompt_lists) + len(self.decode_lists) + len(self.decode_batch.schedule_lists) != 0 or (
-            self.num_schedule_decode + self.num_schedule_prefill) != 0
+        return len(self.prompt_lists) + len(self.decode_lists) + len(self.decode_batch.schedule_lists) != 0 or self.num_schedule_prefill != 0
 
     def get_memory_util(self):
         return round((self.total_num_free_pages - self.num_free_pages)*100 / self.total_num_free_pages, 2)
