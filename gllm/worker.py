85ba046ad99be40419000540b3b8cf27d9732823
gtyinstinct <guoty9@mail2.sysu.edu.cn>
2025-03-18 11:12:39 +0800

    Fix prefill token budget

--- gllm/worker.py
+++ gllm/worker.py
@@ -155,12 +155,13 @@ class Worker:
         schedule_decode_seqs = []
         
         # prefill
-        prefill_token_budget = max(self.get_num_free_pages()-self.num_threshold_free_pages,0)
+        prefill_token_budget = self.page_size * max(self.get_num_free_pages()-self.num_threshold_free_pages,0)
         if self.pp_size > 1:
             prefill_token_budget = min(
                 round(self.model_runner.memory_manager.get_memory_free() * self.max_batch_tokens),
                 prefill_token_budget)
-        
+        else:
+            prefill_token_budget = min(self.max_batch_tokens, prefill_token_budget)
         prefill_batched_token_nums = 0
         for seq in self.seqs_to_prefill:
             if prefill_token_budget == 0:
