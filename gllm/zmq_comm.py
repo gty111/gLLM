3a7f38c5c18a863086c76269255db2c9d3d77e0f
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-04-25 16:43:26 +0800

    Refactor zmqcomm (#36)

---
+++ gllm/zmq_comm.py
@@ -0,0 +1,96 @@
+import zmq
+import pickle
+
+from gllm.utils import make_socket
+
+class zmqComm:
+    def __init__(self, pp_rank, pp_size, schedule_path, output_path, token_path):
+        self.pp_rank = pp_rank
+        self.pp_size = pp_size
+        self.schedule_path = schedule_path
+        self.output_path = output_path
+        self.token_path = token_path
+
+    def init(self):
+        self.ctx = zmq.Context()
+        if self.pp_rank == 0:
+            # front-end => rank 0
+            self.request_socket = make_socket(
+                self.ctx, self.schedule_path, zmq.PULL)
+            # rank 0 => front-end
+            self.output_socket = make_socket(
+                self.ctx, self.output_path, zmq.PUSH)
+            if self.pp_size != 1:
+                # rank 0 => other ranks : batched seqs
+                self.schedule_sockets = []
+                for i in range(1, self.pp_size):
+                    self.schedule_sockets.append(make_socket(
+                        self.ctx, f'{self.schedule_path}_{i}', zmq.PUSH))
+                # last rank => rank 0 : next tokens
+                self.token_socket = make_socket(
+                    self.ctx, self.token_path, zmq.PULL)
+        else:
+            # rank 0 => other ranks : batched seqs
+            self.schedule_socket = make_socket(
+                self.ctx, f'{self.schedule_path}_{self.pp_rank}', zmq.PULL)
+
+        if self.pp_rank == self.pp_size - 1 and self.pp_size != 1:
+            # last rank => rank 0 : next tokens
+            self.token_socket = make_socket(
+                self.ctx, self.token_path, zmq.PUSH)
+
+        if self.pp_size == 0:  # front-end process
+            self.request_socket = make_socket(
+                self.ctx, self.schedule_path, zmq.PUSH)
+            self.output_socket = make_socket(
+                self.ctx, self.output_path, zmq.PULL)
+
+    def send_tokens(self, tokens):
+        assert type(tokens) == list
+        tokens_bytes = pickle.dumps(tokens)
+        self.token_socket.send(tokens_bytes, copy=False)
+
+    def recv_tokens(self):
+        if self.token_socket.poll(timeout=0) != 0:
+            recv_bytes = self.token_socket.recv(copy=False)
+            next_tokens = pickle.loads(recv_bytes)
+            return next_tokens
+        else:
+            return None
+
+    def send_output(self, output):
+        output_bytes = pickle.dumps(output)
+        self.output_socket.send(output_bytes, copy=False)
+
+    def recv_output(self):
+        if self.output_socket.poll(timeout=0) != 0:
+            recv_bytes = self.output_socket.recv(copy=False)
+            output = pickle.loads(recv_bytes)
+            return output
+        else:
+            return None
+
+    def send_schedule(self, seqs):
+        seqs_bytes = pickle.dumps(seqs)
+        for socket in self.schedule_sockets:
+            socket.send(seqs_bytes, copy=False)
+
+    def recv_schedule(self):
+        if self.schedule_socket.poll(timeout=0) != 0:
+            recv_bytes = self.schedule_socket.recv(copy=False)
+            seqs = pickle.loads(recv_bytes)
+            return seqs
+        else:
+            return None
+
+    def send_requests(self, ipc_package):
+        ipc_bytes = pickle.dumps(ipc_package)
+        self.request_socket.send(ipc_bytes, copy=False)
+
+    def recv_requests(self):
+        if self.request_socket.poll(timeout=0) != 0:
+            recv_bytes = self.request_socket.recv(copy=False)
+            ipc_package = pickle.loads(recv_bytes)
+            return ipc_package
+        else:
+            return None
