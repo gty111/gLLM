e734044ecf5e65ea3587c1a3c1ee95c7dc606f11
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-06-01 17:01:33 +0800

    Use fused rmsnorm kernel for qwen3 (#67)
    * Use fused rmsnorm for qwen3
    
    * Add check in rms_norm

--- gllm/models/qwen3.py
+++ gllm/models/qwen3.py
@@ -41,11 +41,11 @@ class Qwen3Attention(nn.Module):
         # Add qk-norm
         q_by_head = q.view(*q.shape[:-1], q.shape[-1] // self.head_dim,
                            self.head_dim)
-        q_by_head = self.q_norm.forward_native(q_by_head)
+        q_by_head = self.q_norm(q_by_head)
         q = q_by_head.view(q.shape)
         k_by_head = k.view(*k.shape[:-1], k.shape[-1] // self.head_dim,
                            self.head_dim)
-        k_by_head = self.k_norm.forward_native(k_by_head)
+        k_by_head = self.k_norm(k_by_head)
         k = k_by_head.view(k.shape)
         q, k = self.rotary_emb(input_data.positions, q, k)
         attn_output = self.attn.forward(q, k, v, input_data)
