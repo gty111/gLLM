cb4cad97b90afd4797d4b0c88cf5097154fb4562
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-06-01 09:46:22 +0800

    Remove redundant dtype setting (#65)

--- gllm/dist_utils.py
+++ gllm/dist_utils.py
@@ -3,27 +3,6 @@ import torch
 
 from logger import logger
 
-def send_tensor(tensor:torch.Tensor, dst):
-    tensor_shape = list(tensor.shape)
-    # send dim
-    dist.send_object_list([len(tensor_shape)],dst)
-    # send shape
-    dist.send_object_list(tensor_shape,dst)
-    # send tensor
-    dist.send(tensor,dst)
-    
-def recv_tensor(dtype, src):
-    # recv dim
-    dim = [None]
-    dist.recv_object_list(dim,src)
-    # recv shape
-    tensor_shape = [None for _ in range(dim[0])]
-    dist.recv_object_list(tensor_shape,src)
-    # recv tensor
-    tensor = torch.zeros(torch.Size(tensor_shape),dtype=dtype,device=f'cuda:{get_local_rank()}')
-    dist.recv(tensor,src)
-    return tensor
-
 def send_pp_data(output, dst):
     if type(output) == tuple:
         assert len(output) == 2
@@ -32,8 +11,8 @@ def send_pp_data(output, dst):
     else:
         dist.isend(output,dst)
 
-def recv_pp_data(src, dtype, shape, has_residual):
-    hidden_states = torch.zeros(torch.Size(shape),dtype=dtype,device=f'cuda:{get_local_rank()}')
+def recv_pp_data(src, shape, has_residual):
+    hidden_states = torch.zeros(torch.Size(shape))
     if has_residual:
         residual = hidden_states.clone().detach()
         hidden_states_future = dist.irecv(hidden_states,src)
