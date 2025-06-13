7c4123f1fa117d6431c3a6bef887feab52a69fa5
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-06-13 16:30:51 +0800

    Make llama compatible for tp

--- gllm/models/llama.py
+++ gllm/models/llama.py
@@ -3,29 +3,19 @@ import torch
 from typing import Optional
 from torch import nn
 
-from gllm.layers.activation import SiluAndMul
+from gllm.layers.linear import RowParallelLinear, QKVParallelLinear
 from gllm.layers.layernorm import RMSNorm
 from gllm.layers.rotary_embedding import RotaryEmbedding, LinearScalingRotaryEmbedding, Llama3RotaryEmbedding
 from gllm.layers.attention import FlashAttention
 from gllm.input_data import InputData
-from gllm.dist_utils import get_pp_layers, get_pp_rank, get_local_rank, is_last_pp_rank, resolve_pp_layer
-from gllm.utils import get_model_load_pbar
+from gllm.dist_utils import get_pp_layers, get_pp_rank, is_last_pp_rank, get_tp_size
 
+from .qwen2 import Qwen2MLP, Qwen2ForCausalLM
 
-class LlamaMLP(nn.Module):
+class LlamaMLP(Qwen2MLP):
 
     def __init__(self, config):
-        super().__init__()
-        self.hidden_size = config.hidden_size
-        self.intermediate_size = config.intermediate_size
-        self.gate_up_proj = nn.Linear(
-            self.hidden_size, self.intermediate_size*2, bias=False)
-        self.down_proj = nn.Linear(
-            self.intermediate_size, self.hidden_size, bias=False)
-        self.act_fn = SiluAndMul()
-
-    def forward(self, x: torch.Tensor):
-        return self.down_proj(self.act_fn(self.gate_up_proj(x)))
+        super().__init__(config, False)
 
 
 class LlamaAttention(nn.Module):
@@ -33,14 +23,39 @@ class LlamaAttention(nn.Module):
     def __init__(self, layer_id: int, config):
         super().__init__()
         self.hidden_size = config.hidden_size
-        self.num_heads = config.num_attention_heads
-        self.head_dim = self.hidden_size // self.num_heads
-        self.num_key_value_heads = config.num_key_value_heads
+        
+        tp_size = get_tp_size()
+        
+        self.total_num_heads = config.num_attention_heads
+        assert self.total_num_heads % tp_size == 0
+        self.num_heads = self.total_num_heads // tp_size
+        
+        self.total_num_kv_heads = config.num_key_value_heads
+        if self.total_num_kv_heads >= tp_size:
+            # Number of KV heads is greater than TP size, so we partition
+            # the KV heads across multiple tensor parallel GPUs.
+            assert self.total_num_kv_heads % tp_size == 0
+        else:
+            # Number of KV heads is less than TP size, so we replicate
+            # the KV heads across multiple tensor parallel GPUs.
+            assert tp_size % self.total_num_kv_heads == 0
+        self.num_kv_heads = max(1, self.total_num_kv_heads // tp_size)
+        
+        self.head_dim = self.hidden_size // self.total_num_heads
 
-        self.qkv_proj = nn.Linear(self.hidden_size, (self.num_heads+self.num_key_value_heads*2)
-                                  * self.head_dim, bias=False)
-        self.o_proj = nn.Linear(self.num_heads*self.head_dim,
-                                self.hidden_size, bias=False)
+        self.qkv_proj = QKVParallelLinear(
+            self.hidden_size,
+            self.head_dim,
+            self.total_num_heads,
+            self.total_num_kv_heads,
+            bias=False
+        )
+        
+        self.o_proj = RowParallelLinear(
+            self.total_num_heads * self.head_dim,
+            self.hidden_size,
+            bias=False
+        )
         
         self.rope_theta = getattr(config,'rope_theta',10000)
         
@@ -70,10 +85,10 @@ class LlamaAttention(nn.Module):
         self.scaling = self.head_dim**-0.5
 
         self.attn = FlashAttention(
-            layer_id, self.scaling, self.num_heads, self.num_key_value_heads, self.head_dim, self.hidden_size)
+            layer_id, self.scaling, self.num_heads, self.num_kv_heads, self.head_dim, self.hidden_size)
 
         self.q_size = self.num_heads * self.head_dim
-        self.kv_size = self.num_key_value_heads * self.head_dim
+        self.kv_size = self.num_kv_heads * self.head_dim
 
     def forward(self, input_data: InputData, hidden_states: torch.Tensor):
         qkv = self.qkv_proj(hidden_states)
@@ -145,59 +160,7 @@ class LlamaModel(nn.Module):
             return hidden_states, residual
 
 
-class LlamaForCausalLM(nn.Module):
+class LlamaForCausalLM(Qwen2ForCausalLM):
 
     def __init__(self, config):
-        super().__init__()
-        self.max_model_len = config.max_position_embeddings
-        self.num_kv_heads = config.num_key_value_heads
-        self.head_dim = config.hidden_size // config.num_attention_heads
-        self.model = LlamaModel(config)
-        self.num_layers = len(self.model.layers)
-        self.config = config
-        self.ret_residual = True
-        if is_last_pp_rank():
-            self.lm_head = nn.Linear(
-                config.hidden_size, config.vocab_size, bias=False)
-
-    def forward(self, input_data: InputData, hidden_states=None, residual=None):
-        return self.model(input_data, hidden_states, residual)
-
-    def compute_logits(self, input_data: InputData, hidden_states: torch.Tensor):
-        # fetch hidden_states of last token in each seq
-        idx_list = input_data.query_start_loc - 1
-        return self.lm_head(hidden_states[idx_list[1:]])
-
-    def load_weights(self, weights, mp_load_progress):
-        parameters = dict(self.named_parameters())
-        if mp_load_progress is not None:
-            mp_load_progress[get_local_rank()*2] = len(parameters)
-            mp_load_progress[get_local_rank()*2+1] = 0
-        else:
-            pbar = get_model_load_pbar(len(parameters))
-
-        # assert len(parameters) == len(weights)
-        num_attn_heads = self.config.num_attention_heads
-        head_dim = self.config.hidden_size // num_attn_heads
-        num_kv_heads = self.config.num_key_value_heads
-        intermediate_size = self.config.intermediate_size
-        for k, v in parameters.items():
-            k = resolve_pp_layer(k, 2, self.model.start_layer)
-            if k.find('self_attn.qkv_proj') != -1:
-                v.data[:num_attn_heads*head_dim, :] = weights[k.replace(
-                    'qkv_proj', 'q_proj')]
-                v.data[num_attn_heads*head_dim:(num_attn_heads +
-                       num_kv_heads)*head_dim, :] = weights[k.replace('qkv_proj', 'k_proj')]
-                v.data[(num_attn_heads +
-                       num_kv_heads)*head_dim:, :] = weights[k.replace('qkv_proj', 'v_proj')]
-            elif k.find('gate_up_proj') != -1:
-                v.data[:intermediate_size, :] = weights[k.replace(
-                    'gate_up_proj', 'gate_proj')]
-                v.data[intermediate_size:, :] = weights[k.replace(
-                    'gate_up_proj', 'up_proj')]
-            else:
-                v.data.copy_(weights[k])
-            if mp_load_progress is not None:
-                mp_load_progress[get_local_rank()*2+1] += 1
-            else:
-                pbar.update(1)
+        super().__init__(config, LlamaModel)
