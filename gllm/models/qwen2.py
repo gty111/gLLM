168e263b1e25d3c1e6370c38980de523f00501a5
guoty <guoty9@mail2.sysu.edu.cn>
2025-07-15 07:33:42 +0800

    Fix weights loading

--- gllm/models/qwen2.py
+++ gllm/models/qwen2.py
@@ -207,7 +207,7 @@ class Qwen2ForCausalLM(nn.Module):
                 copy_gate_up_proj_weight(v.data,
                                          weights[k.replace('gate_up_proj', 'gate_proj')],
                                          weights[k.replace('gate_up_proj', 'up_proj')],
-                                         v.shape[0]//get_tp_size())
+                                         v.shape[0]//2)
             elif k.find('down_proj') != -1:
                 copy_single_proj_col(v.data, weights[k], v.shape[1])
             elif k.find('embed_tokens') != -1 or k.find('lm_head') != -1:
